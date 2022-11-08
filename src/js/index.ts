f4190dd7954c646523f601252dd4211a9aad6848
Kristján Oddsson <koddsson@gmail.com>
2022-11-08 21:06:23 +0100

    Implement network monitor

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,29 @@
-// Put the code here
+const list = document.querySelector('#events')!
+
+// Get the connection type.
+let type = navigator.connection.type
+
+// Register for event changes:
+navigator.connection.addEventListener('change', function () {
+  if (type !== type) {
+    log(`Connection type changed from ${type} to ${navigator.connection.effectiveType}`)
+    type = navigator.connection.effectiveType
+  }
+})
+
+function log(text: string): void {
+  const item = document.createElement('li')
+  item.textContent = text
+  list.append(item)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (navigator.onLine) {
+    log("We're online!")
+  } else {
+    log("We're offline!")
+  }
+})
+
+window.addEventListener('offline', () => log('The network connection has been lost.'))
+window.addEventListener('online', () => log('The network connection has been established.'))
