f0e4a6b0cbf4321fe27028b7b29ffb4f20615d7f
Kristján Oddsson <koddsson@gmail.com>
2022-11-14 13:41:22 +0100

    Check if document is ready before adding `DOMContentLoaded` listener

--- src/js/index.ts
+++ src/js/index.ts
@@ -17,15 +17,15 @@ function log(text: string): void {
   list.prepend(item)
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  if (navigator.onLine) {
-    log("We're online!")
-    document.body.style.backgroundColor = 'var(--green-5)'
-  } else {
-    log("We're offline!")
-    document.body.style.backgroundColor = 'var(--red-5)'
-  }
-})
+async function ready(): Promise<void> {
+  return new Promise(resolve => {
+    if (document.readyState === 'complete' || document.readyState === 'interactive') {
+      resolve()
+    } else {
+      document.addEventListener('DOMContentLoaded', () => resolve())
+    }
+  })
+}
 
 window.addEventListener('online', () => {
   log('The network connection has been established.')
@@ -36,3 +36,14 @@ window.addEventListener('offline', () => {
   log('The network connection has been lost.')
   document.body.style.backgroundColor = 'var(--red-5)'
 })
+
+await ready()
+if (navigator.onLine) {
+  log("We're online!")
+  document.body.style.backgroundColor = 'var(--green-5)'
+} else {
+  log("We're offline!")
+  document.body.style.backgroundColor = 'var(--red-5)'
+}
+
+export {}
